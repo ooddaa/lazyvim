eeb9594ba1b5543f362ec3c238575396816bbe7d
dmitry-vikhorev <dmitryvikhorev@storyteq.com>
2025-02-25 17:37:05 +0000

    lspconfig and remaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,22 @@ local map = vim.keymap.set
 
 map("n", "k", "10k")
 map("n", "l", "10j")
+map("v", "L", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Rebind Grep from <leader>/ to <leader>fw
+-- We'll use the same function that's assigned to <leader>sg since that's a grep function too
+map("n", "<leader>fw", "<leader>sg", { remap = true, desc = "Grep (Root Dir)" })
+
+-- Add mapping to close current buffer with <leader>z
+map("n", "<leader>z", function()
+  vim.cmd("bdelete")
+end, { desc = "Close current buffer" })
+
+-- Normal mode comment toggle
+-- This maps <leader>/ to the same functionality as the gc operator
+map("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment line" })
+
+-- Visual mode comment toggle
+-- This maps <leader>/ to the same functionality as the gc operator in visual mode
+map("v", "<leader>/", "gc", { remap = true, desc = "Toggle comment selection" })
